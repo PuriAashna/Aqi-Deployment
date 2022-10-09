3262528f8c6f037c80e8524add041cc2108d5d61
PuriAashna <90599413+PuriAashna@users.noreply.github.com>
2022-10-09 17:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 import pickle
 import sklearn
-import sklearn.ensemble._forest
+import sklearn.ensemble.forest
 
 # load the model from disk
 loaded_model=pickle.load(open('random_forest_regression_model1.pkl', 'rb'))
